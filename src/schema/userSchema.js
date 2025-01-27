0e78b03c7beef7017e4dcc378ea3c407daffed3f
vaibhav-chittora <CHITTORAVAIBHAV70@GMAIL.COM>
2025-01-27 17:19:24 +0530

    implemented user schema

---
+++ src/schema/userSchema.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone number is required"],
+    },
+    role: {
+      type: String,
+      enum: ["client", "admin", "vendor"],
+      default: "client",
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
